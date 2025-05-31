134c3b4be14bf32d2ef1830e5d8398ea31b49683
manimehalaianandaraj <manimehalaiannakodi@gmail.com>
2025-05-31 22:04:42 +0530

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import UserList from './UserList';
+import UserDetails from './UserDetails';
 
-import React from "react";
-import './../styles/App.css';
-
-const App = () => {
+function App() {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+    <Routes>
+      <Route path="/" element={<UserList />} />
+      <Route path="/users/:id" element={<UserDetails />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
